1ddc4afed0019685a2285e1bfa9ef19b32239594
mristic505 <mateja.ristic@gmail.com>
2023-07-17 09:48:05 -0400

    add bi directional example

---
+++ src/step-03-bi-directional/nextApp/next.config.js
@@ -0,0 +1,32 @@
+const NextFederationPlugin = require('@module-federation/nextjs-mf/lib/NextFederationPlugin');
+
+module.exports = {
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: 'nextApp',
+          remotes: {
+            remote: 'reactApp@http://localhost:8080/remoteEntry.js',
+          },
+          filename: 'static/chunks/remoteEntry.js',
+          exposes: {            
+            './nextjs-layout-box': './components/nextjs-layout-box.js'
+          },
+          shared: {
+            // react: {
+            //   requiredVersion: false,
+            //   singleton: true,
+            // },
+          },
+          extraOptions: {
+            skipSharingNextInternals: true,
+          },
+        }),
+      );
+    }
+    return config;
+  },
+  // your original next.config.js export
+  reactStrictMode: true,
+};
---
+++ src/step-03-bi-directional/reactApp/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LayoutBox from 'remote/nextjs-layout-box';
+
+function App() {
+  return (
+    <>      
+      <LayoutBox>
+        <div>This is the React container App hosted at localhost:8080</div>
+      </LayoutBox>
+    </>
+  );
+}
+
+export default App;
