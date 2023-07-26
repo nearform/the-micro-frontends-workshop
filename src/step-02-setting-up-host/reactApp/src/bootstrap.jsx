552cfd5e389d4ecc7bf173d36aad3146e884c1d4
Mateja Ristic <mateja.ristic@gmail.com>
2023-07-26 16:03:06 -0400

    Update src/step-02-setting-up-host/reactApp/src/bootstrap.jsx
    Co-authored-by: Russ Bredihin <bredikhin@users.noreply.github.com>

--- src/step-02-setting-up-host/reactApp/src/bootstrap.jsx
+++ src/step-02-setting-up-host/reactApp/src/bootstrap.jsx
@@ -2,5 +2,5 @@ import App from './App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 root.render(<App />);
