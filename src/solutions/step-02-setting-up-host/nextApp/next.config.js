0d721e9b444fe870e438b1928ae5701651bc4287
Russ Bredihin <bredikhin@users.noreply.github.com>
2023-08-07 15:50:41 -0400

    chore: address code review suggestions

--- src/solutions/step-02-setting-up-host/nextApp/next.config.js
+++ src/solutions/step-02-setting-up-host/nextApp/next.config.js
@@ -1,9 +1,7 @@
-// Uncomment and configure
 const NextFederationPlugin = require('@module-federation/nextjs-mf')
 
 module.exports = {
   webpack(config) {
-    // Place your NextFederationPlugin config here
     config.plugins.push(
       new NextFederationPlugin({
         name: 'nextApp',
