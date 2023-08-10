cc3b625e5c24cd170d06139a59f136af5fe063d5
Russ Bredihin <bredikhin@users.noreply.github.com>
2023-08-10 19:12:04 -0400

    fix: downgrade the next.js module federation plugin and add an exception to the dependabot config (#98)

--- src/solutions/step-03-bidirectional/nextApp/next.config.js
+++ src/solutions/step-03-bidirectional/nextApp/next.config.js
@@ -1,34 +1,31 @@
 const NextFederationPlugin = require('@module-federation/nextjs-mf')
 
 module.exports = {
-  webpack(config, options) {
-    if (!options.isServer) {
-      config.plugins.push(
-        new NextFederationPlugin({
-          name: 'nextApp',
-          remotes: {
-            remote: 'reactApp@http://localhost:8080/remoteEntry.js',
+  webpack(config) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'nextApp',
+        remotes: {
+          remote: 'reactApp@http://localhost:8080/remoteEntry.js',
+        },
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './nextjs-layout-box': './components/nextjs-layout-box.js',
+          './nextjs-table': './components/nextjs-table.js',
+        },
+        shared: {
+          react: {
+            requiredVersion: false,
+            singleton: true,
           },
-          filename: 'static/chunks/remoteEntry.js',
-          exposes: {
-            './nextjs-layout-box':
-              './components/nextjs-layout-box.js',
-            './nextjs-table': './components/nextjs-table.js',
-          },
-          shared: {
-            react: {
-              requiredVersion: false,
-              singleton: true,
-            },
-          },
-          extraOptions: {
-            skipSharingNextInternals: true,
-          },
-        })
-      )
-    }
+        },
+        extraOptions: {
+          skipSharingNextInternals: true,
+        },
+      })
+    )
+
     return config
   },
-  // your original next.config.js export
   reactStrictMode: true,
 }
