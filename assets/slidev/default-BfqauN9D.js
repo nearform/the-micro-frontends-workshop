c75993858f5e339f15ad0ee429181df6c88b508c
simoneb <simoneb@users.noreply.github.com>
2025-08-18 11:16:51 +0000

    deploy: 0d1b0b67e3e0adef19e81579251f0d0a5155ed31

--- assets/slidev/default-BfqauN9D.js
+++
@@ -1,1 +0,0 @@
-import{o,b as t,f as s}from"../modules/vue-BzsTFDC4.js";import{_ as r}from"../index-BT2WEJ27.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
